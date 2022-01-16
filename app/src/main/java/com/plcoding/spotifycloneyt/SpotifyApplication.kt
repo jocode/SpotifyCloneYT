6574488482b5d2ccb77ed1e3e1026e6df1eb3b09
Johan Mosquera <joncamilomoni@gmail.com>
2022-01-16 11:47:35 -0500

    Dagger-Hilt Setup

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/SpotifyApplication.kt
@@ -0,0 +1,11 @@
+package com.plcoding.spotifycloneyt
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication : Application() {
+
+
+
+}
